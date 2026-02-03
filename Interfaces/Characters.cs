6cae44175a3391b5652fe33a1ff5c9612451ccf8
bryelle.kimball <bryelle.kimball@ENGR8KCP5S3>
2026-02-03 13:53:58 -0700

    Add project files.

---
+++ Interfaces/Characters.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Characters
+{
+    interface IMovable
+    {
+        void Move();
+    }
+    public class Player : IMovable
+    {
+        public void Move() 
+        {
+            Console.WriteLine("Player walks confidently, unafraid.");
+        }
+    }
+    public class Enemy : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Enemy stalks menacingly, ready to pounce.");
+        }
+    }
+    public class NPC : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("NPC glitches and starts walking in the wall.");
+        }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,10 @@
+﻿using Characters;
+
+
+IMovable player = new Player();
+IMovable enemy = new Enemy();
+IMovable nPC = new NPC();
+
+player.Move();
+enemy.Move();
+nPC.Move();
